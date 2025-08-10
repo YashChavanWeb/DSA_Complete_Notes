6d8a937b89dc5a6f20b345e47dc8b1f7a4646e5c
YashChavanWeb <yashchavan4628@gmail.com>
2025-08-10 17:41:22 +0530

    flood fill fixed

--- Programs/13_Graphs/06_flood_fill.cpp
+++ Programs/13_Graphs/06_flood_fill.cpp
@@ -4,23 +4,23 @@ using namespace std;
 void dfs(int row, int col, vector<vector<int>> &res, vector<vector<int>> &image,
          int iniColor, int newColor, int drow[], int dcol[])
 {
+    // Fill the current cell with the new color
     res[row][col] = newColor;
 
     int n = image.size();
     int m = image[0].size();
 
+    // Explore all 4 directions (up, right, down, left)
     for (int i = 0; i < 4; ++i)
     {
         int nrow = row + drow[i];
         int ncol = col + dcol[i];
 
-        if (
-            nrow >= 0 && nrow < n &&
-            ncol >= 0 && ncol < m &&
+        // Check if the new cell is valid and has the initial color
+        if (nrow >= 0 && nrow < n && ncol >= 0 && ncol < m &&
             image[nrow][ncol] == iniColor && res[nrow][ncol] != newColor)
         {
-
-            res[nrow][ncol] = newColor;
+            // Recursively call dfs for the new cell
             dfs(nrow, ncol, res, image, iniColor, newColor, drow, dcol);
         }
     }
@@ -28,25 +28,23 @@ void dfs(int row, int col, vector<vector<int>> &res, vector<vector<int>> &image,
 
 vector<vector<int>> floodFill(vector<vector<int>> image, int sr, int sc, int newColor)
 {
-
     vector<vector<int>> res = image;
 
     int iniColor = image[sr][sc];
 
+    // No need to do anything if the initial color is the same as the new color
+    if (iniColor == newColor)
+        return res;
+
     int n = image.size();
     int m = image[0].size();
 
-    // deltas
+    // Directional deltas for up, right, down, left
     int drow[] = {-1, 0, 1, 0};
     int dcol[] = {0, 1, 0, -1};
 
-    for (int i = 0; i < n; ++i)
-    {
-        for (int j = 0; j < m; ++j)
-        {
-            dfs(sr, sc, res, image, iniColor, newColor, drow, dcol);
-        }
-    }
+    // Start the flood fill from the initial position
+    dfs(sr, sc, res, image, iniColor, newColor, drow, dcol);
 
     return res;
 }
